1023857cabe91ee5be5000729b5780b69ffe3b75
Jirayu Kanda <me@acodify.com>
2016-06-07 16:34:46 +0700

    insert field modifierId in table

--- src/SystemLogs/Logs.php
+++ src/SystemLogs/Logs.php
@@ -32,12 +32,13 @@ class Logs
         return $data = $query->result();
     }
 
-    public function setLog($id_ref = '', $key = '', $logs = '')
+    public function setLog($id_ref = '', $key = '', $logs = '', $modifyById = 0)
     {
         $this->db->set('id_ref', $id_ref);
         $this->db->set('key', $key);
         $this->db->set('logs', $logs);
         $this->db->set('created_on', date("Y-m-d H:i:s"));
+        $this->db->set('modifier_id', $modifyById);
         $this->db->insert('weg_status_logs');
     }
 
